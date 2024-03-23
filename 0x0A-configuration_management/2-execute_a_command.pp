ffb76f9582497f13a432df1eb1c59b6bd2daf3bd
Esra-333 <esra.omer444@gmail.com>
2024-03-23 02:25:52 -0700

    a

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # kill process killmenow
 
 exec { 'pkill':
- command  => 'pkill killmenow',
- provider => 'shell',
+ command  => 'pkill -9 -f killmenow',
+ path     => ['/usr/bin', '/usr/sbin', '/bin']
 }
